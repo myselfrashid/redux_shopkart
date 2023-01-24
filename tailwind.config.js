139ee9be4dc48e346a36d2a50b602c9288c0a947
myselfrashid <rashidali.jmi@gmail.com>
2023-01-24 23:28:11 +0530

    added cart and store management

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Noto:['Noto Sans', 'sans-serif'],
+      },
+    },
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+  },
+  plugins: [],
+}
